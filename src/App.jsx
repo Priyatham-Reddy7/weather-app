a7ccac31ba2116e9a5e0b9d16242efd3b2c71206
Priyatham Reddy <priyathamreddy147@gmail.com>
2022-07-22 22:15:48 +0530

    Mapped data from weather api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 import "./App.css"
 import Search from "./components/search/Search"
 import CurrentWeather from "./components/currentWeather/CurrentWeather"
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api"
+import { useState } from "react"
 
 const App = () => {
+    const [currentWeather, setCurrentWeather] = useState(null)
+    const [forecast, setForecast] = useState(null)
+
     const handleOnSearchChange = (searchData) => {
-        console.log(searchData)
+        //splits the input into latitude and longitude
+        const [lat, lon] = searchData.value.split(" ")
+
+        //Fetches current weather using lat and lon attributes
+        const currentWeatherFetch = fetch(
+            `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+        )
+
+        //Fetches forecast
+        const forecastFetch = fetch(
+            `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+        )
+
+        Promise.all([currentWeatherFetch, forecastFetch])
+            .then(async (response) => {
+                const weatherResponse = await response[0].json()
+                const foreacastResponse = await response[1].json()
+                setCurrentWeather({
+                    city: searchData.label,
+                    ...weatherResponse,
+                })
+                setForecast({ city: searchData.label, ...foreacastResponse })
+            })
+            .catch((err) => console.log(err))
     }
 
     return (
         <div className="container">
             <Search onSearchChange={handleOnSearchChange} />
-            <CurrentWeather />
+            {currentWeather && <CurrentWeather data={currentWeather} />}
         </div>
     )
 }
--- src/components/currentWeather/CurrentWeather.jsx
+++ src/components/currentWeather/CurrentWeather.jsx
@@ -1,37 +1,50 @@
 import "./CurrentWeather.css"
-import icon from "../../assets/icons/01d.png"
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
+    console.log(data)
     return (
         <div className="weather">
             <div className="top">
                 <div>
-                    <p className="city">Tenali</p>
-                    <p className="weather-description">Sunny</p>
+                    <p className="city">{data.city}</p>
+                    <p className="weather-description">
+                        {data.weather[0].description}
+                    </p>
                 </div>
-                <img src={icon} alt="weather-icon" />
+                <img
+                    src={`../../src/assets/icons/${data.weather[0].icon}.png`}
+                    alt="weather-icon"
+                />
             </div>
             <div className="bottom">
-                <p className="temperature">35°C</p>
+                <p className="temperature">{Math.round(data.main.temp)}°C</p>
                 <div className="details">
                     <div className="parameter-row">
                         <span className="parameter-label">Details</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Feels like</span>
-                        <span className="parameter-value">40°C</span>
+                        <span className="parameter-value">
+                            {Math.round(data.main.feels_like)}°C
+                        </span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Wind</span>
-                        <span className="parameter-value">4 m/s</span>
+                        <span className="parameter-value">
+                            {data.wind.speed} m/s
+                        </span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Humidity</span>
-                        <span className="parameter-value">20%</span>
+                        <span className="parameter-value">
+                            {data.main.humidity}%
+                        </span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Pressure</span>
-                        <span className="parameter-value">25 hPa</span>
+                        <span className="parameter-value">
+                            {data.main.pressure} hPa
+                        </span>
                     </div>
                 </div>
             </div>
