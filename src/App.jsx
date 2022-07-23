086056df37a505d7466faa682fb95074bc9b7e67
Priyatham Reddy <priyathamreddy147@gmail.com>
2022-07-23 15:41:12 +0530

    Add Forecast comp

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/search/Search"
 import CurrentWeather from "./components/currentWeather/CurrentWeather"
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api"
 import { useState } from "react"
+import Forecast from "./components/forecast/Forecast"
 
 const App = () => {
     const [currentWeather, setCurrentWeather] = useState(null)
@@ -35,10 +36,13 @@ const App = () => {
             .catch((err) => console.log(err))
     }
 
+    console.log(forecast)
+
     return (
         <div className="container">
             <Search onSearchChange={handleOnSearchChange} />
             {currentWeather && <CurrentWeather data={currentWeather} />}
+            {forecast && <Forecast data={forecast} />}
         </div>
     )
 }
